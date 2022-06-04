da2158b275e6231c30d031eee32d6da38c77bfe0
jinwook song <wlsdnr129@naver.com>
2022-06-04 17:57:34 +0900

    Lifecyle onMount, onDestroy

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+
+	import Something from './Something.svelte'
+
+	let showSomething = false
+</script>
+
+<button on:click={() => {showSomething = !showSomething}}>Toggle</button>
+
+{#if showSomething}
+	<Something />
+{/if}
---
+++ src/Something.svelte
@@ -0,0 +1,22 @@
+<script>
+import { onDestroy, onMount } from "svelte";
+
+
+onMount(() => {
+    console.log('mounted')
+    // return을 통해서도 onDestroy 가능
+    return () => {
+        console.log('destoried!')
+    }
+})
+
+onDestroy(() => {
+    console.log('Before destoried')
+    // Destory 이전에 실행되기 떄문에 component를 읽을 수 있음
+    const h1 = document.querySelector('h1')
+    console.log(h1.innerText)
+})
+</script>
+
+
+<h1>Something</h1>

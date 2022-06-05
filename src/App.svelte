bab2ff80e592f98c52e59f2064670256e8927cd6
jinwook song <wlsdnr129@naver.com>
2022-06-05 18:34:42 +0900

    key block

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,12 @@
 <script>
-  import Example from './each-pattern.svelte';
+  import Example from './key-block.svelte';
+
+  let reset = false;
 </script>
 
-<Example />
+<!-- Key Block은 연결된 데이터의 값이 변경될때마다 내용을 파괴하고 다시 랜더링 -->
+{#key reset}
+  <Example />
+{/key}
+
+<button on:click={() => (reset = !reset)}>Reset</button>
---
+++ src/key-block.svelte
@@ -0,0 +1,6 @@
+<script>
+  let count = 0;
+  setInterval(() => count++, 1000);
+</script>
+
+<div>{count}</div>
